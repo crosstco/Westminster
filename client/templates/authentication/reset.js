68d14043f0d9dec1f0da3dbe93d11567b3e85175
Sanjeet Suhag <suhagsanjeet@gmail.com>
2017-07-28 21:15:08 -0400

    Fixes formatting and adds doumentation to reset.js

--- client/templates/authentication/reset.js
+++ client/templates/authentication/reset.js
@@ -1,26 +1,20 @@
-import {Accounts} from 'meteor/accounts-base'
+/*
+ * Template to reset the password, in case a user forgets their login password.
+ */
 
+import {Accounts} from 'meteor/accounts-base'
 
 Template.reset.events({
   "submit #reset-form": function (e) {
     e.preventDefault();
     var userEmail = $("#reset-email").val();
-    
-  		
-  Accounts.forgotPassword({email:userEmail},function(err){
-        if (err) {
-          window.alert("Email doesn't exist. Please check your typing.");
-          //console.log('Error: ',err);
-          return;
-
-        }
-        else {
-          console.log('Email sent');
-          Router.go('login');
-
-        }
-      });
-    }}); 
-
-
-  
+    Accounts.forgotPassword({email:userEmail},function(err) {
+      if (err) {
+        window.alert("Unregistered email.");
+        return;
+      } else {
+        Router.go('login');
+      }
+    });
+  }
+});
