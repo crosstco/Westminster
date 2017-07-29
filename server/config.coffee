89801540976be975bf1303c21ce9931d3180fa30
Sanjeet Suhag <suhagsanjeet@gmail.com>
2017-07-28 21:45:41 -0400

    Fixes Reset Password Email configuration.

--- server/config.coffee
+++ server/config.coffee
@@ -2,11 +2,5 @@ Accounts.config
   sendVerificationEmail: true
   forbidClientAccountCreation: false
 
-Accounts.emailTemplates.siteName = "blabla.com"
-Accounts.emailTemplates.from = "blabla.com site Admin <admin@blabla.com>"
-
-Accounts.emailTemplates.verifyEmail.subject = (user) ->
-  "Confirm registration on " + Accounts.emailTemplates.siteName;
-
-Accounts.emailTemplates.verifyEmail.text = (user, url) ->
-   "To confirm your registration, please, click this link " + url 
+Accounts.emailTemplates.siteName = "Westminster Portal"
+Accounts.emailTemplates.from = "Westminster Portal <brainbootcamps1@gmail.com>"
--- server/startup/fixtures.js
+++ server/startup/fixtures.js
@@ -1,27 +1,10 @@
 Meteor.startup(function () {
 
-  process.env.MAIL_URL="smtps://brainbootcamps1%40gmail.com:lrhlrh90@smtp.gmail.com:465/"; 
+  process.env.MAIL_URL="smtps://brainbootcamps1%40gmail.com:lrhlrh90@smtp.gmail.com:465/";
 
   Accounts.emailTemplates.siteName = Meteor.settings.siteName;
   Accounts.emailTemplates.from = Meteor.settings.emailFrom;
+  Accounts.emailTemplates.resetPassword.subject = function (user) {
+    return "Westminster Portal - Password Reset";
+  };
 });
-
-// Welcome and Email Verification
-
-Accounts.emailTemplates.verifyEmail.subject = function(user) {
-  return 'Welcome to Funkytown.io :-)';
-};
-Accounts.emailTemplates.verifyEmail.html = function (user, url) {
-  // return html string
-  return Handlebars.templates.verifyEmail_html({
-    emailAddress: user.email(),
-    url: url,
-  });
-};
-Accounts.emailTemplates.verifyEmail.text = function (user, url) {
-  // return text string
-  return Handlebars.templates.verifyEmail_text({
-    emailAddress: user.email(),
-    url: url,
-  });
-};
